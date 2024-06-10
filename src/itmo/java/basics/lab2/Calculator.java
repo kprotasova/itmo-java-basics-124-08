3e63361d1e18998bac076872541413086c239c3f
Ekaterina Protasova <ge48nir@mytum.de>
2024-06-10 16:29:02 +0200

    lab2 done

---
+++ src/itmo/java/basics/lab2/Calculator.java
@@ -0,0 +1,51 @@
+package itmo.java.basics.lab2;
+
+public class Calculator {
+    public int sum (int a, int b) {
+        return a+b;
+    }
+
+    public double sum (double a, double b) {
+        return a+b;
+    }
+
+    public long sum (long a, long b) {
+        return a+b;
+    }
+
+    public int mul (int a, int b) {
+        return a*b;
+    }
+
+    public double mul (double a, double b) {
+        return a*b;
+    }
+
+    public long mul (long a, long b) {
+        return a*b;
+    }
+
+    public int div (int a, int b) {
+        return a/b;
+    }
+
+    public double div (double a, double b) {
+        return a/b;
+    }
+
+    public long div (long a, long b) {
+        return a/b;
+    }
+
+    public int sub (int a, int b) {
+        return a-b;
+    }
+
+    public double sub (double a, double b) {
+        return a-b;
+    }
+
+    public long sub (long a, long b) {
+        return a-b;
+    }
+}
---
+++ src/itmo/java/basics/lab2/Human.java
@@ -0,0 +1,20 @@
+package itmo.java.basics.lab2;
+
+public class Human {
+    private byte age;
+    private String name;
+    private String sex;
+    private int height;
+    private int weight;
+
+    public Human() {};
+
+    public Human (byte age, String name, String sex, int height, int weight) {
+        this.age = age;
+        this.name = name;
+        this.sex = sex;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+
